7541d0a4bf0cfac3202cb9c6820ab7038a6f858b
Prem <premdasvm8@gmail.com>
2024-03-01 11:00:40 +0530

    Typegoose added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypegooseModule } from '@m8a/nestjs-typegoose';
 
 @Module({
-  imports: [],
+  imports: [
+    TypegooseModule.forRoot('mongodb://prem:vespheroo7@localhost:27017/', {}),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
