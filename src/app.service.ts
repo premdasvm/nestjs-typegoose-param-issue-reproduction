85b45421f5130e11043b1ff310b9243937d4bf60
Prem <premdasvm8@gmail.com>
2024-03-01 10:54:40 +0530

    Sample route added.

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  returnStudentId(studentId: number): string {
+    return `Student ID: ${studentId} `;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { swaggerOptions } from './swagger.plugin';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger Setup
+
+  const options = new DocumentBuilder()
+    .setTitle('NestJS-Typegoose')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options, {});
+
+  SwaggerModule.setup('doc', app, document, {
+    explorer: true,
+    swaggerOptions,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
